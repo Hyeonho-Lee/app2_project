611b8e9d729ea72a8d660f9d8258be2c58789c38
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2022-06-03 11:40:05 +0900

    Update main.dart

--- Screen/main.dart
+++ Screen/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'home_screen.dart';
+import 'notice_screen.dart';
+import 'profile_screen.dart';
+import 'alert_screen.dart';
+import 'login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,7 +16,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/notice': (context) => NoticeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/alert': (context) => AlertScreen(),
+        '/login': (context) => LoginScreen(),
+      },
     );
   }
 }
