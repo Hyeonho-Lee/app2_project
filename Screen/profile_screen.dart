9df977a2485cfd46bca23c4c359893df11e3b404
leehyeonho <54760301+Hyeonho-Lee@users.noreply.github.com>
2022-06-03 11:41:34 +0900

    Create profile_screen.dart

---
+++ Screen/profile_screen.dart
@@ -0,0 +1,168 @@
+import 'home_screen.dart';
+import '/model/user_model.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('프로필'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pushReplacementNamed('/');
+          },
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Center(
+              child: Container(
+                width: 500,
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 0,
+                      blurRadius: 5.0,
+                      offset: Offset(0, 10),
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.grey,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      '닉네임 님\n어서오세요!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 13,
+                        letterSpacing: 0.1,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.justify,
+                      //overflow: TextOverflow.ellipsis,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                  width: 500,
+                  height: 400,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            //print("공지사항 클릭");
+                            Navigator.of(context).pushReplacementNamed('/notice');
+                          },
+                          child: Text(
+                            '공지사항 ▣',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 22,
+                              letterSpacing: 0.1,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        TextButton(
+                          onPressed: () {
+                            //print("프로필 클릭");
+                          },
+                          child: Text(
+                            '프로필 ▣',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 22,
+                              letterSpacing: 0.1,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        TextButton(
+                          onPressed: () {
+                            //print("알림 클릭");
+                            Navigator.of(context).pushReplacementNamed('/alert');
+                          },
+                          child: Text(
+                            '알림 ▣',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 22,
+                              letterSpacing: 0.1,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Container(
+                color: Color(0xffffff),
+                height: MediaQuery.of(context).size.height/1.5,
+                child: Padding(
+                  padding: const EdgeInsets.all(42.0),
+                  child: Text('test'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
